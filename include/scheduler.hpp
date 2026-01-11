46f991dcded1cb4e02bd1f30a81f14300217cd27
Pistold <119545844+Pistold@users.noreply.github.com>
2026-01-10 17:19:17 -0800

    Update scheduler.hpp
    more methods to header file

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -12,10 +12,22 @@ struct Task {
     std::chrono::steady_clock::time_point next_run; // Next scheduled time
 };
 
-// Simple cooperative scheduler (embedded-style)
+// Simple cooperative scheduler (embedded)
 class Scheduler {
 public:
-    // Add a task that runs every `period`
+
+    Scheduler() = default;
+    ~Scheduler() = default;
+
+    // Prevent copying
+    Scheduler(const Scheduler&) = delete;
+    Scheduler& operator=(const Scheduler&) = delete;
+
+    // Allow move movement if needed
+    Scheduler(Scheduler&&) = default;
+    Scheduler& operator=(Scheduler&&) = default;
+    
+    // Add a task that runs every period
     void add_task(const std::function<void()>& task,
                   std::chrono::milliseconds period);
 
